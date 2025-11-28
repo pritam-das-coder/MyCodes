95065527986782de45f375fd174adb184e91fc84
Pritam-coder969 <pdascse24@gmail.com>
2025-11-28 08:50:50 +0000

    Nov 28, 2025, 2:20 PM

--- python/Image_Processing(OpenCV)/Image_Fundamental/basics.py
+++ python/Image_Processing(OpenCV)/Image_Fundamental/basics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-img=cv2.imread('flower.jpg')
-img=cv2.imwrite('flower.png',img)
+img=cv2.imread('G:\My Drive\MyCodes\python\Image_Processing(OpenCV)\Image_Fundamental\flower.jpg')
+img=cv2.imwrite('G:\My Drive\MyCodes\python\Image_Processing(OpenCV)\Image_Fundamental\flower.png',img)
 cv2.imshow('original',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
