28b4eff71caaf3448a7e7d416fbde5f8319f2e48
Pritam-coder969 <pdascse24@gmail.com>
2025-12-01 08:13:17 +0000

    Dec 1, 2025, 1:43 PM

---
+++ c/DSA_Assignment4/Q4.c
@@ -0,0 +1,3 @@
+// Insert the keys 12, 18, 33, 2, 3, 22, 5, and 15 into a hash table of size 11 using h(k)=
+// k mod 11 and linear probing.Implement the idea of search for the user given input in
+// this table considering collisions as well.
