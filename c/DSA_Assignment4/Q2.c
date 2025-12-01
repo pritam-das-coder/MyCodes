38485793ea4631a33ec8326de312676a58406d21
Pritam-coder969 <pdascse24@gmail.com>
2025-12-01 06:54:35 +0000

    Dec 1, 2025, 12:24 PM

---
+++ c/DSA_Assignment4/Q2.c
@@ -0,0 +1,9 @@
+// Write a program in C to implement a hashing scheme .
+// Given a set of keys, a hash table size, and a hash function (like (k%11)),
+// demonstrate how to insert the keys using different techniques (e.g., linear
+// probing) and show the final state of the hash table.
+// Example: Insert the keys 12, 18, 33, 2, 3, 22, 5, and 15 into a hash table
+// of size 11 using h(k)= k mod 11 and linear probing.
+// h(12)=12%11=1. Insert 12 at index 1.
+// Continue this process for all keys.
+// Print the total no of collisions and load factor.
