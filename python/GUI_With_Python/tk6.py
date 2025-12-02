dd1544887b38eebbc38b962fdaa28a94c7fb86cd
Pritam-coder969 <pdascse24@gmail.com>
2025-12-02 04:53:03 +0000

    Dec 2, 2025, 10:23 AM

--- python/GUI_With_Python/tk6.py
+++ python/GUI_With_Python/tk6.py
@@ -4,7 +4,7 @@ root=Tk()
 
 root.geometry("500x300")
 
-photo=PhotoImage(file=r"G:\My Drive\MyCodes\python\GUI_With_Python\design.png")
+photo=PhotoImage(file=r"G:\My Drive\MyCodes\python\GUI_With_Python\flower.png")
 
 label=Label(image=photo)
 label.pack()
