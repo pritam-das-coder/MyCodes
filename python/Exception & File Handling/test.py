72792537b9fe60507f4ff89b0935d98dc6b8575e
Pritam-coder969 <pdascse24@gmail.com>
2025-12-02 05:45:39 +0000

    Dec 2, 2025, 11:15 AM

--- python/Exception & File Handling/test.py
+++ python/Exception & File Handling/test.py
@@ -1,6 +1,6 @@
 # import my_module as m
-# from my_module import cube,mul,sum
-from my_module import *
+from my_module import cube,mul
+# from my_module import *
 print(cube(4))
 print(mul(2,3))
-print(sum(3,5))
+# print(sum(3,5))
