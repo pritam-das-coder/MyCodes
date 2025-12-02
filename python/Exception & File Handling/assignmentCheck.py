f0a7d82d691e1c26d0a5d60a4cf6871f618ded22
Pritam-coder969 <pdascse24@gmail.com>
2025-12-02 05:49:33 +0000

    Dec 2, 2025, 11:19 AM

--- python/Exception & File Handling/assignmentCheck.py
+++
@@ -1,8 +0,0 @@
-'''python
-    try:
-        x=10/0
-    except ZeroDivisionError:
-        print("Error: Division by Zero")
-    except:
-        print("Other error")
-'''
--- python/Exception & File Handling/test.py
+++ python/Exception & File Handling/test.py
@@ -1,6 +1,7 @@
 # import my_module as m
-from my_module import cube,mul
+# from my_module import cube,mul
 # from my_module import *
+from my_package import *
 print(cube(4))
 print(mul(2,3))
 # print(sum(3,5))
