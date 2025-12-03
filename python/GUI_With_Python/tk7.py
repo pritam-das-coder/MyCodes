2d996b4274c4d82ea6d65511b95df3d8ba694fea
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 07:45:51 +0000

    Dec 3, 2025, 1:15 PM

--- python/GUI_With_Python/tk7.py
+++ python/GUI_With_Python/tk7.py
@@ -4,4 +4,12 @@ root=Tk()
 root.geometry("444x233")
 root.title("My GUI With Pritam")
 
+# Important Label Parameters
+# text -> adds the text
+# bd -> background
+# fg -> foreground
+# font -> sets the font
+# padx -> x padding
+# pady -> y padding
+# relief -> border styling - SUNKEN, RAISED, GROOVE, RIDGE
 root.mainloop()
