2a1addcb5b69be1033a18cf9cd914cf6cd442480
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 08:45:46 +0000

    Dec 3, 2025, 2:15 PM

--- python/GUI_With_Python/hw7.py
+++ python/GUI_With_Python/hw7.py
@@ -4,7 +4,7 @@ root=Tk()
 root.geometry("500x400")
 root.title("My Website")
 
-strip_label=Label(text="Ready!",bg="blue",fg="white")
+strip_label=Label(text="Ready!",bg="blue",fg="white",font=("Times New Roman", 15 ,"bold"))
 strip_label.pack(side="bottom",fill=X)
 
 root.mainloop()
