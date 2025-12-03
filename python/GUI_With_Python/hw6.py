4900790581f67ee65a51451f8ee71cbb4a1f1830
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 06:36:15 +0000

    Dec 3, 2025, 12:06 PM

--- python/GUI_With_Python/hw6.py
+++ python/GUI_With_Python/hw6.py
@@ -6,13 +6,13 @@ root=Tk()
 root.geometry("400x300")
 root.title("Photo Album")
 
-image_folder=r""
+image_folder=r"C:\Users\daspr\OneDrive\Pictures\Web Photos"
 files_present=os.listdir(image_folder)
 
 image_list=[]
 
 for file in files_present:
-    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
+    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".webp"):
 
         full_path=os.path.join(image_folder,file)
 
