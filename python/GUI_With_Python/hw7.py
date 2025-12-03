42eff8035d8f6761d019d043ddc29e94b0e91f31
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 08:38:16 +0000

    Dec 3, 2025, 2:08 PM

--- python/GUI_With_Python/hw7.py
+++ python/GUI_With_Python/hw7.py
@@ -2,8 +2,9 @@ from tkinter import *
 
 root=Tk()
 root.geometry("500x400")
+root.title("My Website")
 
 strip_label=Label(text="Ready!",bg="pink")
-strip_label.pack(side="bottom")
+strip_label.pack(side="bottom",fill=X)
 
 root.mainloop()
