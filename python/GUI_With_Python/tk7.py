3e6848b21da9af7ed51eefd6eb6ae5b1b1cee870
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 07:58:53 +0000

    Dec 3, 2025, 1:28 PM

--- python/GUI_With_Python/tk7.py
+++ python/GUI_With_Python/tk7.py
@@ -6,8 +6,8 @@ root.title("My GUI With Pritam")
 
 # Important Label Parameters
 # text -> adds the text
-# bd -> background
-# fg -> foreground
+# bg -> background
+# fg -> foreground/ writing colour
 # font -> sets the font
 # padx -> x padding
 # pady -> y padding
@@ -15,7 +15,8 @@ root.title("My GUI With Pritam")
 
 title_label = Label(text="The Republic of India, also known as Bharat, is a federal parliamentary republic in " \
 "South Asia and the world's most populous country and largest democracy.\n New Delhi is its capital, and Mumbai is its " \
-"largest city by population.")
+"largest city by population.", bg="red", fg="white", padx=113,pady=94)
+
 title_label.pack()
 
 root.mainloop()
