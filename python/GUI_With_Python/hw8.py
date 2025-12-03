195d60ac61e6cc5aab98be94255c6273e936e4cf
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 13:15:16 +0000

    Dec 3, 2025, 6:45 PM

--- python/GUI_With_Python/hw8.py
+++ python/GUI_With_Python/hw8.py
@@ -3,11 +3,15 @@ from tkinter import *
 from PIL import Image,ImageTk
 
 root=Tk()
-root.geometry("800x")
+root.geometry("800x400")
 root.title("My Newspaper GUI")
 
 newspaper_name=Label(text="The Telegraph")
 
+img=Image.open(r"G:\My Drive\MyCodes\python\GUI_With_Python\vk.jpg")
+tk_img=ImageTk.PhotoImage(img)
+
+l=Label(img=tk_img,text="Hello")
 # folder=r""
 # files_list=os.listdir(folder)
 
