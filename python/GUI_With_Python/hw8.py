2d8bf12df6047ad16c876c6460f7461c59871ec4
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 13:16:47 +0000

    Dec 3, 2025, 6:46 PM

--- python/GUI_With_Python/hw8.py
+++ python/GUI_With_Python/hw8.py
@@ -11,7 +11,8 @@ newspaper_name=Label(text="The Telegraph")
 img=Image.open(r"G:\My Drive\MyCodes\python\GUI_With_Python\vk.jpg")
 tk_img=ImageTk.PhotoImage(img)
 
-l=Label(img=tk_img,text="Hello")
+l=Label(image=tk_img,text="Hello")
+l.pack()
 # folder=r""
 # files_list=os.listdir(folder)
 
