afa6fffb7dd1db72713e5b05d6fa8c22925440bf
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 13:12:41 +0000

    Dec 3, 2025, 6:42 PM

--- python/GUI_With_Python/hw8.py
+++ python/GUI_With_Python/hw8.py
@@ -1,10 +1,17 @@
+import os
 from tkinter import *
 from PIL import Image,ImageTk
 
 root=Tk()
+root.geometry("800x")
 root.title("My Newspaper GUI")
 
 newspaper_name=Label(text="The Telegraph")
+
+# folder=r""
+# files_list=os.listdir(folder)
+
+# for file in files_list:
 
 
 root.mainloop()
