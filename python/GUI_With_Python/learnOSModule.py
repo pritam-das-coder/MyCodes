8aa484001139fc0059adb5d0b716c21e58bc54c1
Pritam-coder969 <pdascse24@gmail.com>
2025-12-03 06:43:38 +0000

    Dec 3, 2025, 12:13 PM

--- python/GUI_With_Python/learnOSModule.py
+++ python/GUI_With_Python/learnOSModule.py
@@ -1,19 +1,8 @@
 import os
-# from tkinter import *
-# from PIL import Image, ImageTk
 
 # print(f"Initial : {os.getcwd()}")
 # os.chdir(r"G:\My Drive\MyCodes\python\GUI_With_Python")
 # print(f"Final : {os.getcwd()}")
 
-# root=Tk()
-
-# image=Image.open("vk.jpg")
-# photo=ImageTk.PhotoImage(image)
-
-# label=Label(image=photo)
-# label.pack()
-
-# root.mainloop()
 # print(os.listdir(os.getcwd()))
 print(os.path.join(r"G:\My Drive\MyCodes\python",r"GUI_With_Python"))
