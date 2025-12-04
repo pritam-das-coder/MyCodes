f0c59fdb1c6adb2341ae2c5a132fbdabef1addc5
Pritam-coder969 <pdascse24@gmail.com>
2025-12-04 07:27:59 +0000

    Dec 4, 2025, 12:57 PM

--- python/GUI_With_Python/tk10.py
+++ python/GUI_With_Python/tk10.py
@@ -7,6 +7,15 @@ f=Frame(root,bg="grey",borderwidth=6,relief=SUNKEN)
 f.pack(side="left", anchor="nw")
 
 b1=Button(f,fg="red",text="Print Now")
-b1.pack()
+b1.pack(side="left")
+
+b2=Button(f,fg="red",text="Print Now")
+b2.pack(side="left",padx=16)
+
+b3=Button(f,fg="red",text="Print Now")
+b3.pack(side="left")
+
+b4=Button(f,fg="red",text="Print Now")
+b4.pack(side="left")
 
 root.mainloop()
