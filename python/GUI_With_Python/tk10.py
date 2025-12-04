d6c2b8309dc6046cceceae563c781e03f95d7cb8
Pritam-coder969 <pdascse24@gmail.com>
2025-12-04 07:30:06 +0000

    Dec 4, 2025, 1:00 PM

--- python/GUI_With_Python/tk10.py
+++ python/GUI_With_Python/tk10.py
@@ -1,12 +1,15 @@
 from tkinter import *
 
+def hello():
+    print("Hello from tkinter")
+    
 root=Tk()
 root.geometry("655x333")
 
 f=Frame(root,bg="grey",borderwidth=6,relief=SUNKEN)
 f.pack(side="left", anchor="nw")
 
-b1=Button(f,fg="red",text="Print Now")
+b1=Button(f,fg="red",text="Print Now",command=hello)
 b1.pack(side="left")
 
 b2=Button(f,fg="red",text="Print Now")
