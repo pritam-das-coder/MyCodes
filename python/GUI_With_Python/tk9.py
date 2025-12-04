bc01de3b4ca216ef4fcf712aa321e71a3e87bd7d
Pritam-coder969 <pdascse24@gmail.com>
2025-12-04 05:57:21 +0000

    Dec 4, 2025, 11:27 AM

--- python/GUI_With_Python/tk9.py
+++ python/GUI_With_Python/tk9.py
@@ -3,7 +3,7 @@ from tkinter import *
 root=Tk()
 root.geometry("655x333")
 f1=Frame(root,bg="grey",borderwidth=6,relief=SUNKEN)
-f1.pack(side="left")
+f1.pack(side="left", fill=Y)
 
 l1=Label(f1, text="Project")
 l1.pack(pady=142)
