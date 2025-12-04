67db7987432434061cbdaf7dc0d91c13207d3d92
Pritam-coder969 <pdascse24@gmail.com>
2025-12-04 06:10:00 +0000

    Dec 4, 2025, 11:40 AM

--- python/GUI_With_Python/tk9.py
+++ python/GUI_With_Python/tk9.py
@@ -4,7 +4,7 @@ root=Tk()
 root.geometry("655x333")
 
 f1=Frame(root,bg="grey",borderwidth=6,relief=SUNKEN)
-f1.pack(side="left", fill=Y)
+f1.pack(side="left", fill=Y,pady=100)
 
 f2=Frame(root,bg="grey",borderwidth=8,relief=SUNKEN)
 f2.pack(side="top",fill=X)
