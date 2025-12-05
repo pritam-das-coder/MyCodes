90fff4585ea6bb8d3df0332151be84de92cef8a8
Pritam-coder969 <pdascse24@gmail.com>
2025-12-05 15:41:21 +0000

    Dec 5, 2025, 9:11 PM

---
+++ python/GUI_With_Python/hw12.py
@@ -0,0 +1,24 @@
+from tkinter import *
+root = Tk()
+root.geometry("600x300")
+
+Label(root,text="Portal").grid(row=0)
+
+username = Label(root,text="Username :")
+password = Label(root,text="Password :")
+
+username.grid(row=1,column=0)
+password.grid(row=2,column=0)
+
+uservalue = StringVar()
+passvalue = StringVar()
+
+username_entry = Entry(root,textvariable=uservalue)
+password_entry = Entry(root,textvariable=passvalue)
+
+username_entry.grid(row=1,column=1)
+password_entry.grid(row=2,column=1)
+
+Button(root,text="Submit",borderwidth=6,bg="green").grid()
+
+root.mainloop()
