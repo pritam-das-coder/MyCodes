a7d483679d84d4bd806a86b91a2ae14d7d0670bb
Pritam-coder969 <pdascse24@gmail.com>
2025-12-05 15:45:30 +0000

    Dec 5, 2025, 9:15 PM

--- python/GUI_With_Python/hw12.py
+++ python/GUI_With_Python/hw12.py
@@ -1,8 +1,12 @@
 from tkinter import *
+
+def fun():
+    print("Values submitted!")
+
 root = Tk()
 root.geometry("600x300")
 
-Label(root,text="Portal").grid(row=0)
+Label(root,text="Portal").grid(row=0,)
 
 username = Label(root,text="Username :")
 password = Label(root,text="Password :")
@@ -19,6 +23,6 @@ password_entry = Entry(root,textvariable=passvalue)
 username_entry.grid(row=1,column=1)
 password_entry.grid(row=2,column=1)
 
-Button(root,text="Submit",borderwidth=6,bg="green").grid()
+Button(root,text="Submit",borderwidth=6,bg="green",command=fun,relief="sunken").grid(row=3,column=1)
 
-root.mainloop()
+root.mainloop(
