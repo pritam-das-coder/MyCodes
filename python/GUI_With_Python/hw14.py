510c9dddd7da150881f8416e2e34c89eb4bab742
Pritam-coder969 <pdascse24@gmail.com>
2025-12-06 13:40:09 +0000

    Dec 6, 2025, 7:10 PM

--- python/GUI_With_Python/hw14.py
+++ python/GUI_With_Python/hw14.py
@@ -2,4 +2,14 @@ from tkinter import *
 
 root=Tk()
 
+canvas_width=800
+canvas_height=400
+
+root.geometry(f"{canvas_width}x{canvas_height}")
+
+canvas_widget=Canvas(root,width=canvas_width,height=canvas_height)
+canvas_widget.pack()
+
+
+
 root.mainloop()
