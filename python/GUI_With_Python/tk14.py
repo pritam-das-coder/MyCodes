dc86cc332dbd3508c613b36faf4339b1ccee7084
Pritam-coder969 <pdascse24@gmail.com>
2025-12-06 13:03:45 +0000

    Dec 6, 2025, 6:33 PM

--- python/GUI_With_Python/tk14.py
+++ python/GUI_With_Python/tk14.py
@@ -7,7 +7,10 @@ canvas_width=800
 canvas_height=400
 
 root.geometry(f"{canvas_width}x{canvas_height}")
+root.title("Canvas Shapes")
 can_widget=Canvas(root,width=canvas_width,height=canvas_height)
 can_widget.pack()
+
+# line goes from the point x1, y1 to x2,y2
 
 root.mainloop()
