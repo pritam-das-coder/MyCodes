845c616fe337fc32033c0de90ec85c21b0feebfc
Pritam-coder969 <pdascse24@gmail.com>
2025-12-06 14:03:14 +0000

    Dec 6, 2025, 7:33 PM

--- python/GUI_With_Python/hw14.py
+++ python/GUI_With_Python/hw14.py
@@ -10,6 +10,7 @@ root.geometry(f"{canvas_width}x{canvas_height}")
 canvas_widget=Canvas(root,width=canvas_width,height=canvas_height)
 canvas_widget.pack()
 
+# end points of main diagonal of rectangle 
 canvas_widget.create_arc(30,40,200,210,start=90,extent=270)
 
 root.mainloop()
