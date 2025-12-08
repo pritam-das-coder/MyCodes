654f22023c99f1dd5efb15bbbe3541ec82932669
Pritam-coder969 <pdascse24@gmail.com>
2025-12-08 14:13:22 +0000

    Dec 8, 2025, 7:43 PM

--- python/GUI_With_Python/practice15.py
+++ python/GUI_With_Python/practice15.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
 def copy(event):
-    widget_text=event.widget
+    wid=event.widget
+    wid_text=wid.cget('text')
+
+    root.clipboard_clear()
+    root.clipboard_append(wid_text)
+
+    root.update()
 
 root=Tk()
 root.geometry("600x300")
@@ -10,6 +16,9 @@ root.title("Learn Tkinter Events")
 lb=Label(root,text="Tumi kotha deyechile Kota je harie gele")
 lb.pack()
 
+entry=Entry()
+entry.pack()
+lb.bind('<Control-c>',copy)
 
 
 
