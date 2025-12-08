2cfb3d2187130e4fd76e0f60e391f17a99d0f5aa
Pritam-coder969 <pdascse24@gmail.com>
2025-12-08 15:29:01 +0000

    Dec 8, 2025, 8:59 PM

--- python/GUI_With_Python/textWidget.py
+++ python/GUI_With_Python/textWidget.py
@@ -4,6 +4,9 @@ root=Tk()
 root.geometry("600x300")
 root.title("Learn Tkinter Text Widget")
 # line_number.character
+# 1.0 -> start
+# 1-> line number starts from 1
+# 0-> after 0th character starts from character 0
 wid=Text(root,height=1,width=20,bd=4)
 wid.insert("1.0","Hi hello bye")
 wid.pack()
