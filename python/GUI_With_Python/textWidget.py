ab239ff5d5462ea5023f764d048bb7e4945eaff5
Pritam-coder969 <pdascse24@gmail.com>
2025-12-08 15:25:39 +0000

    Dec 8, 2025, 8:55 PM

--- python/GUI_With_Python/textWidget.py
+++ python/GUI_With_Python/textWidget.py
@@ -3,7 +3,7 @@ from tkinter import *
 root=Tk()
 root.geometry("600x300")
 root.title("Learn Tkinter Text Widget")
-
+# line_number.character
 wid=Text(root,height=1,width=20,bd=4)
 wid.insert("1.0","Hi hello bye")
 wid.pack()
