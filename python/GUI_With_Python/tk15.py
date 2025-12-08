9c5f2636c861a68dfa4612de88aa783e10414e27
Pritam-coder969 <pdascse24@gmail.com>
2025-12-08 13:45:52 +0000

    Dec 8, 2025, 7:15 PM

--- python/GUI_With_Python/tk15.py
+++ python/GUI_With_Python/tk15.py
@@ -4,6 +4,9 @@ def fun(event):
     print(f"You clicked on the button at {event.x}, {event.y}")
 def hello(event):
     print("Hello")
+def bye(event):
+    print("Bye")
+
 root=Tk()
 root.geometry("600x300")
 root.title("Learn Tkinter Events")
@@ -13,6 +16,8 @@ widget.pack()
 
 widget.bind('<Button-1>',fun)
 widget.bind('<Double-1>',quit)
-widget.bind('<Motion>',hello)
+# widget.bind('<Motion>',hello)
+widget.bind("<Enter>",hello)
+widget.bind("<Leave>",bye)
 
 root.mainloop()
