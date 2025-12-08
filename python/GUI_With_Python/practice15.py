95dd0f523ab26d32767d99b855eddaa01c9f4e54
Pritam-coder969 <pdascse24@gmail.com>
2025-12-08 14:08:13 +0000

    Dec 8, 2025, 7:38 PM

--- python/GUI_With_Python/practice15.py
+++ python/GUI_With_Python/practice15.py
@@ -1,26 +1,16 @@
 from tkinter import *
 
-def fun(event):
-    print(f"You clicked on the button at {event.x}, {event.y}")
-def hello(event):
-    print("Hello")
-def bye(event):
-    print("Bye")
 def copy(event):
     widget_text=event.widget
-    
+
 root=Tk()
 root.geometry("600x300")
 root.title("Learn Tkinter Events")
 
-widget=Button(root,text="Click me")
-widget.pack()
+lb=Label(root,text="Tumi kotha deyechile Kota je harie gele")
+lb.pack()
 
-# widget.bind('<Button-1>',fun)
-# widget.bind('<Double-1>',quit)
-# widget.bind('<Motion>',hello)
-# widget.bind("<Enter>",hello)
-# widget.bind("<Leave>",bye)
+
 
 
 root.mainloop()
