663061bce7b17ea39a6e92074f76162e1ae42864
Pritam-coder969 <pdascse24@gmail.com>
2025-12-10 06:05:34 +0000

    Dec 10, 2025, 11:35 AM

--- python/GUI_With_Python/tk18.py
+++ python/GUI_With_Python/tk18.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+def fun():
+    print("Hello aap kese hai!")
+
 root=Tk()
 root.geometry("600x300")
 root.title("Learn Menus & Submenus")
@@ -8,6 +11,11 @@ mainmenu = Menu(root)
 root.config(menu=mainmenu)
 
 m1 = Menu(mainmenu)
-m1.add_command()
+m1.add_command(label="New project",command=fun)
+m1.add_command(label="Save",command=fun)
+m1.add_command(label="Save As",command=fun)
+m1.add_command(label="Print",command=fun)
+
+mainmenu.add_cascade(label="File",menu=m1)
 
 root.mainloop()
