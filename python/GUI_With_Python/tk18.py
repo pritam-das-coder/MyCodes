cf6604dbee70333d189dc752134e4bf931d1bbf3
Pritam-coder969 <pdascse24@gmail.com>
2025-12-10 06:19:45 +0000

    Dec 10, 2025, 11:49 AM

--- python/GUI_With_Python/tk18.py
+++ python/GUI_With_Python/tk18.py
@@ -7,9 +7,18 @@ root=Tk()
 root.geometry("600x300")
 root.title("Learn Menus & Submenus")
 
+# Use this to create a non dropdown menu
+# mainmenu = Menu(root)
+# root.config(menu=mainmenu)
+# mainmenu.add_command(label="File",command=fun)
+# mainmenu.add_command(label="Exit",command=quit)
+
 mainmenu = Menu(root)
 root.config(menu=mainmenu)
-mainmenu.add_command(label="File",command=fun)
-mainmenu.add_command(label="Exit",command=quit)
+
+m1 = Menu(mainmenu)
+m1.add_command(label="New Text File")
+
+
 
 root.mainloop()
