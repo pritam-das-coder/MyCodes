b1caf1b4a0d70acb46e7c3bd9f9730642ffab035
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 18:36:40 +0000

    Dec 12, 2025, 12:06 AM

--- python/GUI_With_Python/place.py
+++ python/GUI_With_Python/place.py
@@ -6,6 +6,6 @@ root.geometry("600x300")
 # Label(root,text="Mercedes Benz").place(x=0,y=0)
 # Label(root,text="Tata Safari").place(x=50,y=50,anchor=E)
 
-Label(root,text="Mahindra SUV").place(relx=0.5,rely=0.5,anchor=W)
-
+Label(root,text="Mahindra SUV").place(relx=0.5,rely=0.2,anchor="center")
+# default anchor = nw
 root.mainloop()
