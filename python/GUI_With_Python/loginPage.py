2f621601dd136ade064fcd2eb8f87de788b15d5a
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 19:03:16 +0000

    Dec 12, 2025, 12:33 AM

--- python/GUI_With_Python/loginPage.py
+++ python/GUI_With_Python/loginPage.py
@@ -7,5 +7,6 @@ root.title("Login System")
 f0=Frame(root,relief="groove",borderwidth=5)
 f0.place(relx=0.5,rely=0.5,anchor="center",relheight=0.6,relwidth=0.6)
 
+Label(f0,text="Username").place(relx=0.1,rely=0.1,anchor="center")
 
 root.mainloop()
