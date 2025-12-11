cc3d5f8482613a69b541d475efb7cae6ba661e2d
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 14:13:36 +0000

    Dec 11, 2025, 7:43 PM

--- python/GUI_With_Python/tk20.py
+++ python/GUI_With_Python/tk20.py
@@ -4,7 +4,7 @@ import tkinter.messagebox as tmsg
 def get_dollar():
     print(f"We have credited {myslider.get()} dollars to your bank account")
     tmsg.showinfo("Amount Credited!",f"We have credited {myslider.get()} dollars to your bank account")
-    
+
 root=Tk()
 root.geometry("700x400")
 root.title("Learn Slider")
@@ -13,7 +13,7 @@ root.title("Learn Slider")
 # myslider.pack()
 Label(root,text="How many dollars do you want?").pack()
 
-myslider = Scale(root,from_=0,to=100,orient=HORIZONTAL)
+myslider = Scale(root,from_=0,to=100,orient=HORIZONTAL,tickinterval=25)
 myslider.pack()
 
 Button(root,text="Get Dollars!",relief="raised",borderwidth=4,command=get_dollar).pack(pady=10)
