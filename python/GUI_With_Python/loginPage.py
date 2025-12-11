aed308661e3c64df38feec15d00777db7f99c3fc
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 18:57:21 +0000

    Dec 12, 2025, 12:27 AM

--- python/GUI_With_Python/loginPage.py
+++ python/GUI_With_Python/loginPage.py
@@ -4,8 +4,8 @@ root=Tk()
 root.geometry("600x350")
 root.title("Login System")
 
-f0=Frame(root,)
-
+f0=Frame(root,relief="groove",bg="red")
+f0.place(relx=0.5,rely=0.5,anchor="center",relheight=0.5,relwidth=0.5)
 
 
 root.mainloop()
