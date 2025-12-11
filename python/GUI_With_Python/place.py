b12535621a55fb74226f9c9a2b67058738b08258
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 18:45:54 +0000

    Dec 12, 2025, 12:15 AM

--- python/GUI_With_Python/place.py
+++ python/GUI_With_Python/place.py
@@ -3,11 +3,11 @@ from tkinter import *
 root=Tk()
 root.geometry("600x300")
 
-# Label(root,text="Mercedes Benz").place(x=0,y=0)
+Label(root,text="Mercedes Benz",bg="red").place(x=0,y=0,width=130,height=50)
 # Label(root,text="Tata Safari").place(x=50,y=50,anchor=E)
 
 # Label(root,text="Mahindra SUV",bg="red").place(relx=0.5,rely=0.5,anchor="center")
 # default anchor = nw
 
-Label(root,text="Cars",bg="red").place(relx=0.5,rely=0,relheight=1,relwidth=0.5)
+# Label(root,text="Cars",bg="red").place(relx=0.5,rely=0,relheight=1,relwidth=0.5)
 root.mainloop()
