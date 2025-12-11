f57126065a9cf5a9786a95dfeaeade3ccdf50e03
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 07:14:18 +0000

    Dec 11, 2025, 12:44 PM

--- python/GUI_With_Python/tk19.py
+++ python/GUI_With_Python/tk19.py
@@ -1,11 +1,19 @@
 from tkinter import *
 
+def fun():
+    print("Coding is fun")
+
 root=Tk()
 root.geometry("700x350")
 
 mainmenu = Menu(root)
 root.config(menu=mainmenu)
 
-m1 = Menu(mainmenu)
+m1 = Menu(mainmenu,tearoff=0)
 m1.add_command(label="New File",command=fun)
+m1.add_command(label="Save",command=fun)
+m1.add_command(label="Copy",command=fun)
+
+mainmenu.add_cascade(label="File",menu=m1)
+
 root.mainloop()
