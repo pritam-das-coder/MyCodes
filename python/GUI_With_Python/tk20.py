b22982330354c4a20e9bb1faa245a099747153ca
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 14:03:08 +0000

    Dec 11, 2025, 7:33 PM

--- python/GUI_With_Python/tk20.py
+++ python/GUI_With_Python/tk20.py
@@ -6,8 +6,11 @@ root.title("Learn Slider")
 
 # myslider=Scale(root,from_=0,to=100) # vertical
 # myslider.pack()
+Label(root,text="How many dollars do you want?").pack()
 
 myslider = Scale(root,from_=0,to=100,orient=HORIZONTAL)
 myslider.pack()
+
+Button(root,text="Get Dollars!",relief=SUNKEN).pack(pady=10)
 
 root.mainloop()
