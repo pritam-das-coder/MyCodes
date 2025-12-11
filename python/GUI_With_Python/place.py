9a983853b60121aaa3dfb54ab52f348a8fbd7709
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 18:26:17 +0000

    Dec 11, 2025, 11:56 PM

--- python/GUI_With_Python/place.py
+++ python/GUI_With_Python/place.py
@@ -4,6 +4,6 @@ root=Tk()
 root.geometry("600x300")
 
 Label(root,text="Mercedes Benz").place(x=0,y=0)
-Label(root,text="Tata Safari").place(x=50,y=50)
+Label(root,text="Tata Safari").place(x=50,y=50,anchor=E)
 
 root.mainloop()
