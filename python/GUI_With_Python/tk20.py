48863a9bfe87fa7af593507f2809c3562498963a
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 13:57:10 +0000

    Dec 11, 2025, 7:27 PM

--- python/GUI_With_Python/tk20.py
+++ python/GUI_With_Python/tk20.py
@@ -4,4 +4,10 @@ root=Tk()
 root.geometry("700x400")
 root.title("Learn Slider")
 
+# myslider=Scale(root,from_=0,to=100) # vertical
+# myslider.pack()
+
+myslider = Scale(root,from_=0,to=100,orient=HORIZONTAL)
+myslider.pack()
+
 root.mainloop()
