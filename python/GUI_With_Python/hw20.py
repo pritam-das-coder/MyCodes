82e473ad1b40a0e3a709cfc4b8e97841d46130c9
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 14:28:18 +0000

    Dec 11, 2025, 7:58 PM

--- python/GUI_With_Python/hw20.py
+++ python/GUI_With_Python/hw20.py
@@ -6,5 +6,9 @@ root.title("Restaurant Feedback")
 
 Label(root,text="Thank you for coming to our restaurant.\nPlease give us a rating.").pack()
 
-slider =Scale()
+slider = Scale(root,from_=0,to=10,orient="horizontal")
+slider.pack()
+
+Button(root,text="Submit",relief="raised",borderwidth=5).pack(pady=10)
+
 root.mainloop()
