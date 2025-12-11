018898fc20ac0b776578d8fac3360e119762698d
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 19:08:51 +0000

    Dec 12, 2025, 12:38 AM

--- python/GUI_With_Python/loginPage.py
+++ python/GUI_With_Python/loginPage.py
@@ -8,5 +8,10 @@ f0=Frame(root,relief="groove",borderwidth=5)
 f0.place(relx=0.5,rely=0.5,anchor="center",relheight=0.6,relwidth=0.6)
 
 Label(f0,text="Username").place(relx=0.2,rely=0.3,anchor="center")
-Label(f0,text="Password").place(relx=0.2,rely=0.3,anchor="center")
+Label(f0,text="Password").place(relx=0.2,rely=0.6,anchor="center")
+
+passvalue=StringVar()
+uservalue=StringVar()
+
+Entry(f0,textvariable=uservalue).place(relx=0.2,rely=)
 root.mainloop()
