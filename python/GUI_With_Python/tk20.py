f12f2b4540ef620eaeed67fbb93b7e5c04356eda
Pritam-coder969 <pdascse24@gmail.com>
2025-12-11 14:10:36 +0000

    Dec 11, 2025, 7:40 PM

--- python/GUI_With_Python/tk20.py
+++ python/GUI_With_Python/tk20.py
@@ -4,6 +4,7 @@ import tkinter.messagebox as tmsg
 def get_dollar():
     print(f"We have credited {myslider.get()} dollars to your bank account")
     tmsg.showinfo("Amount Credited!",f"We have credited {myslider.get()} dollars to your bank account")
+    
 root=Tk()
 root.geometry("700x400")
 root.title("Learn Slider")
