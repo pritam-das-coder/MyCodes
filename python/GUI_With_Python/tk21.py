a1e5495a227c09f092904a739111658351bea166
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 03:10:20 +0000

    Dec 12, 2025, 8:40 AM

---
+++ python/GUI_With_Python/tk21.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root=Tk()
+root.geometry("700x400")
+root.title("Learn Radiobutton")
+
+Label(root,text="What would you like to have sir?",font="lucida 19 bold").pack()
+
+root.mainloop()
