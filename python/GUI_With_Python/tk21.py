429428cb540140b925bd5b41020c8a1c5d994386
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 03:34:18 +0000

    Dec 12, 2025, 9:04 AM

--- python/GUI_With_Python/tk21.py
+++ python/GUI_With_Python/tk21.py
@@ -6,4 +6,8 @@ root.title("Learn Radiobutton")
 
 Label(root,text="What would you like to have sir?",font="lucida 19 bold").pack()
 
+var=IntVar()
+
+Radiobutton(root,text="Dosa",variable=var,value=1).pack()
+
 root.mainloop()
