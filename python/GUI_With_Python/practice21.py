51d191cb1b7e326e1006ed380700628591d7c499
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 06:59:05 +0000

    Dec 12, 2025, 12:29 PM

--- python/GUI_With_Python/practice21.py
+++ python/GUI_With_Python/practice21.py
@@ -22,11 +22,12 @@ root.title("Learn Radiobutton")
 # root.minsize(400,250)
 # root.maxsize(400,250)
 
-wid=Canvas(root)
-wid.pack()
-wid.create_line(10,10,5,10)
 
 Label(root,text="What would you like to have sir?",font="lucida 19 bold",padx=14).pack()
+
+wid=Canvas(root)
+wid.pack()
+wid.create_line(0,5,400,5)
 
 food_list=["Dosa","Idly","Roti","Fish","Fried Rice"]
 var_list=[]
