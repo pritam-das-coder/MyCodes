a6f67e7d24634b5cc781f6c6968f7420163f407d
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 04:33:57 +0000

    Dec 12, 2025, 10:03 AM

--- python/GUI_With_Python/tk21.py
+++ python/GUI_With_Python/tk21.py
@@ -9,7 +9,7 @@ root=Tk()
 root.geometry("700x400")
 root.title("Learn Radiobutton")
 
-Label(root,text="What would you like to have sir?",font="lucida 19 bold").pack()
+Label(root,text="What would you like to have sir?",font="lucida 19 bold",justify="left").pack()
 
 # var=IntVar()
 var = StringVar()
