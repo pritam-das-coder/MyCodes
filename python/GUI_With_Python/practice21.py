ee15282914802e4c16246cf1302b6c853bd3d8c3
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 06:37:01 +0000

    Dec 12, 2025, 12:07 PM

--- python/GUI_With_Python/practice21.py
+++ python/GUI_With_Python/practice21.py
@@ -21,30 +21,15 @@ root.maxsize(400,250)
 
 Label(root,text="What would you like to have sir?",font="lucida 19 bold",padx=14).pack()
 
-# var=IntVar()
-var1 = StringVar()
-var2 = StringVar()
-var3 = StringVar()
-var4 = StringVar()
-var1.set("1")
-var2.set("1")
-var3.set("1")
-var4.set("1")
-# var.set(1)
-# print(var.get())
 food_list=["Dosa","Idly","Roti","Fish","Fried Rice"]
 var_list=[]
 for i in range(len(food_list)):
     v=StringVar()
     var_list.append(v)
 
-# Radiobutton(root,text="Dosa",variable=var1,value="dosa",padx=14).pack(anchor="w")
-# Radiobutton(root,text="Idly",variable=var2,value="idly",padx=14).pack(anchor="w")
-# Radiobutton(root,text="Roti",variable=var3,value="roti",padx=14).pack(anchor="w")
-# Radiobutton(root,text="Fish",variable=var4,value="fish",padx=14).pack(anchor="w")
 for i in range(len(food_list)):
     Radiobutton(root,text=food_list[i],variable=var_list[i],value=food_list[i]).pack(anchor="w")
-# print(var.get())
+
 Button(root,text="Order Now",borderwidth=5,relief="raised",command=order).pack()
-# print(var.get())
+
 root.mainloop()
