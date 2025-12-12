0c919f336928182835ee43c004b82dacdfe15033
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 06:25:57 +0000

    Dec 12, 2025, 11:55 AM

--- python/GUI_With_Python/practice21.py
+++ python/GUI_With_Python/practice21.py
@@ -2,11 +2,14 @@ from tkinter import *
 import tkinter.messagebox as tmsg
 
 def order():
-    order=var.get()
-    if order=="Food":
+    order1=var1.get()
+    order2=var2.get()
+    order3=var3.get()
+    order4=var4.get()
+    if order1=="1" and order2=="1" and order3=="1" and order4=="1":
         msg="Please select your order."
     else:
-        msg=f"Your order for {order} has been taken. Thanks for ordering."
+        msg=f"Your order for {order1}{order2}{order3}{order4} has been taken. Thanks for ordering."
     tmsg.showinfo("Order Received",msg)
 
 
@@ -19,15 +22,21 @@ root.maxsize(400,250)
 Label(root,text="What would you like to have sir?",font="lucida 19 bold",padx=14).pack()
 
 # var=IntVar()
-var = StringVar()
-var.set("Food")
+var1 = StringVar()
+var2 = StringVar()
+var3 = StringVar()
+var4 = StringVar()
+var1.set("1")
+var2.set("1")
+var3.set("1")
+var4.set("1")
 # var.set(1)
 # print(var.get())
 food_list=["Dosa","Idly","Roti","Fish","Fried Rice"]
-Radiobutton(root,text="Dosa",variable=var,value="dosa",padx=14).pack(anchor="w")
-Radiobutton(root,text="Idly",variable=var,value="idly",padx=14).pack(anchor="w")
-Radiobutton(root,text="Roti",variable=var,value="roti",padx=14).pack(anchor="w")
-Radiobutton(root,text="Fish",variable=var,value="fish",padx=14).pack(anchor="w")
+Radiobutton(root,text="Dosa",variable=var1,value="dosa",padx=14).pack(anchor="w")
+Radiobutton(root,text="Idly",variable=var2,value="idly",padx=14).pack(anchor="w")
+Radiobutton(root,text="Roti",variable=var3,value="roti",padx=14).pack(anchor="w")
+Radiobutton(root,text="Fish",variable=var4,value="fish",padx=14).pack(anchor="w")
 # for i in range(len(food_list)):
 #     Radiobutton(root,text=food_list[i],variable=var,value=food_list[i]).pack(anchor="w")
 # print(var.get())
