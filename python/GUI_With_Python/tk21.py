df36041444598f2c9b7abea481772a4f7bd82334
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 04:14:29 +0000

    Dec 12, 2025, 9:44 AM

--- python/GUI_With_Python/tk21.py
+++ python/GUI_With_Python/tk21.py
@@ -11,5 +11,6 @@ var=IntVar()
 Radiobutton(root,text="Dosa",variable=var,value=1).pack()
 Radiobutton(root,text="Idly",variable=var,value=2).pack()
 Radiobutton(root,text="Roti",variable=var,value=3).pack()
+Radiobutton(root,text="Fish",variable=var,value=4).pack()
 
 root.mainloop()
