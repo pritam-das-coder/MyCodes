207b936d156acf5d65e85dcc10b28102d7f41cb7
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 04:28:32 +0000

    Dec 12, 2025, 9:58 AM

--- python/GUI_With_Python/tk21.py
+++ python/GUI_With_Python/tk21.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter.messagebox as tmsg
 
 def order():
-    tmsg.showinfo("Restaurant Order",f"Your order has been taken.")
+    tmsg.showinfo("Restaurant Order",f"Your order for {var.get()} has been taken.")
 
 
 root=Tk()
@@ -11,13 +11,15 @@ root.title("Learn Radiobutton")
 
 Label(root,text="What would you like to have sir?",font="lucida 19 bold").pack()
 
-var=IntVar()
-
-Radiobutton(root,text="Dosa",variable=var,value=1).pack()
-Radiobutton(root,text="Idly",variable=var,value=2).pack()
-Radiobutton(root,text="Roti",variable=var,value=3).pack()
-Radiobutton(root,text="Fish",variable=var,value=4).pack()
-print(var.get())
+# var=IntVar()
+var = StringVar()
+# var.set(1)
+# print(var.get())
+Radiobutton(root,text="Dosa",variable=var,value="dosa").pack()
+Radiobutton(root,text="Idly",variable=var,value="idly").pack()
+Radiobutton(root,text="Roti",variable=var,value="roti").pack()
+Radiobutton(root,text="Fish",variable=var,value="fish").pack()
+# print(var.get())
 Button(root,text="Submit",borderwidth=5,relief="raised",command=order).pack()
-
+# print(var.get())
 root.mainloop()
