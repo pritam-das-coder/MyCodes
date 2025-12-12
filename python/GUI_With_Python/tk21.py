f379b587a49acbdb6225a8e16e4e87b613d6f1f2
Pritam-coder969 <pdascse24@gmail.com>
2025-12-12 04:18:06 +0000

    Dec 12, 2025, 9:48 AM

--- python/GUI_With_Python/tk21.py
+++ python/GUI_With_Python/tk21.py
@@ -1,4 +1,9 @@
 from tkinter import *
+import tkinter.messagebox as tmsg
+
+def order():
+    tmsg.showinfo("Restaurant Order",f"Your order has been taken.")
+
 
 root=Tk()
 root.geometry("700x400")
@@ -12,5 +17,7 @@ Radiobutton(root,text="Dosa",variable=var,value=1).pack()
 Radiobutton(root,text="Idly",variable=var,value=2).pack()
 Radiobutton(root,text="Roti",variable=var,value=3).pack()
 Radiobutton(root,text="Fish",variable=var,value=4).pack()
+
+Button(root,text="Submit",borderwidth=5,relief="raised",command=order).pack()
 
 root.mainloop()
