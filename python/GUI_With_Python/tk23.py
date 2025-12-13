ab5d07410283820b18193811080fbff2de756dc5
Pritam-coder969 <pdascse24@gmail.com>
2025-12-13 05:59:42 +0000

    Dec 13, 2025, 11:29 AM

--- python/GUI_With_Python/tk23.py
+++ python/GUI_With_Python/tk23.py
@@ -3,7 +3,9 @@ from tkinter import *
 root=Tk()
 root.geometry("600x350")
 root.title("Learn Scrollbar")
-
+# For connecting scrollbar to a widget
+# 1. widget(yscrollcommand = scrollbar.set)
+# 2. scrollbar.config()
 sb=Scrollbar(root)
 sb.pack()
 
