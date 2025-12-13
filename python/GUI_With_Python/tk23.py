db4387866ab5bd93719373417b72f6fb2caa16f6
Pritam-coder969 <pdascse24@gmail.com>
2025-12-13 06:25:57 +0000

    Dec 13, 2025, 11:55 AM

--- python/GUI_With_Python/tk23.py
+++ python/GUI_With_Python/tk23.py
@@ -8,14 +8,18 @@ root.title("Learn Scrollbar")
 # 1. widget(yscrollcommand = scrollbar.set)
 # 2. scrollbar.config(command = widget.yview)
 
-sb=Scrollbar(root)
-sb.pack(fill=Y,side="right")
+# sb=Scrollbar(root)
+# sb.pack(fill=Y,side="right")
+sb=Scrollbar(root,orient="horizontal")
+sb.pack(fill="x",side="bottom")
 
-lbx = Listbox(root,yscrollcommand=sb.set)
+# lbx = Listbox(root,yscrollcommand=sb.set)
+lbx=Listbox(root,xscrollcommand=sb.set)
 for i in range(344):
     lbx.insert(END,f"Item : {i}")
 lbx.pack(fill="both",padx=22)
 
-sb.config(command=lbx.yview)
+# sb.config(command=lbx.yview)
+sb.config(command=lbx.xview)
 
 root.mainloop()
