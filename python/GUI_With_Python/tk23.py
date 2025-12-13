2d3e5fee1fcf413e1d8760255c3e3d00607ee3ac
Pritam-coder969 <pdascse24@gmail.com>
2025-12-13 06:14:34 +0000

    Dec 13, 2025, 11:44 AM

--- python/GUI_With_Python/tk23.py
+++ python/GUI_With_Python/tk23.py
@@ -14,7 +14,7 @@ sb.pack(fill=Y,side="right")
 lbx = Listbox(root,yscrollcommand=sb.set)
 for i in range(344):
     lbx.insert(END,f"Item : {i}")
-lbx.pack(fill="both")
+lbx.pack(fill="both",padx=22)
 
 sb.config(command=lbx.yview)
 
