331d3cffdb1bd66afbd2b0e12ecbb3a9cc0c0e91
Pritam-coder969 <pdascse24@gmail.com>
2025-12-13 07:34:24 +0000

    Dec 13, 2025, 1:04 PM

--- python/GUI_With_Python/hw23.py
+++ python/GUI_With_Python/hw23.py
@@ -4,12 +4,17 @@ root=Tk()
 root.geometry("600x350")
 root.title("My Text Editor")
 
-sb=Scrollbar(root)
-sb.pack(side="right",fill="y")
+sb1=Scrollbar(root)
+sb1.pack(side="right",fill="y")
+sb2=Scrollbar(root,orient="horizontal")
+sb2.pack(side="bottom",fill="x")
 
-text = Text(root,yscrollcommand=sb.set)
+text = Text(root,yscrollcommand=sb1.set)
 text.pack(fill="both",expand=True)
+text.insert(1,0,"")
 
-sb.config(command=text.yview)
+sb1.config(command=text.yview)
+sb2.config(command=text.xview)
+
 
 root.mainloop()
