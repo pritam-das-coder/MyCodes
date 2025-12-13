98bcd2021b208211d52fb08e14dd692693bbf8d0
Pritam-coder969 <pdascse24@gmail.com>
2025-12-13 07:50:36 +0000

    Dec 13, 2025, 1:20 PM

--- python/GUI_With_Python/hw23.py
+++ python/GUI_With_Python/hw23.py
@@ -6,15 +6,15 @@ root.title("My Text Editor")
 
 sb1=Scrollbar(root)
 sb1.pack(side="right",fill="y")
-sb2=Scrollbar(root,orient="horizontal")
-sb2.pack(side="bottom",fill="x")
+# sb2=Scrollbar(root,orient="horizontal")
+# sb2.pack(side="bottom",fill="x")
 
 text = Text(root,yscrollcommand=sb1.set)
 text.pack(fill="both",expand=True)
-text.insert("1.0","Hello"*100)
+text.insert("1.0","Hello"*1000)
 
 sb1.config(command=text.yview)
-sb2.config(command=text.xview)
+# sb2.config(command=text.xview)
 
 
 root.mainloop()
