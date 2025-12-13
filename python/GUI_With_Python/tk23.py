e09359ac352f0632f53d880c3ed1265fabd28d4c
Pritam-coder969 <pdascse24@gmail.com>
2025-12-13 06:49:27 +0000

    Dec 13, 2025, 12:19 PM

--- python/GUI_With_Python/tk23.py
+++ python/GUI_With_Python/tk23.py
@@ -13,14 +13,17 @@ sb.pack(fill=Y,side="right")
 # sb=Scrollbar(root,orient="horizontal")
 # sb.pack(fill="x",side="bottom")
 
-lbx = Listbox(root,yscrollcommand=sb.set)
+# lbx = Listbox(root,yscrollcommand=sb.set)
 # lbx=Listbox(root,xscrollcommand=sb.set)
-for i in range(344):
-    lbx.insert(END,f"Item : {i}")
+# for i in range(344):
+#     lbx.insert(END,f"Item : {i}")
 # lbx.insert(END,f"This is a very very very very very very very long line that needs scrolling...")
-lbx.pack(fill="both",padx=22)
+# lbx.pack(fill="both",padx=22)
 
-sb.config(command=lbx.yview)
+# sb.config(command=lbx.yview)
 # sb.config(command=lbx.xview)
+
+txt = Text(root)
+
 
 root.mainloop()
