5bb05a874d456e3809bf8c50027e556c3678758c
Pritam-coder969 <pdascse24@gmail.com>
2025-12-18 14:45:17 +0000

    Dec 18, 2025, 8:15 PM

--- c/Code_With_Harry_C_Tutorials/infixToPostfixPrac.c
+++ c/Code_With_Harry_C_Tutorials/infixToPostfixPrac.c
@@ -5,3 +5,19 @@ typedef struct stack{
     int size;
     char* arr;
 }stack;
+
+int stackTop(stack* sp){
+    return sp->arr[sp->top];
+}
+
+int isEmpty(stack* sp){
+    return (sp->top==-1);
+}
+
+int isFull(stack* sp){
+    return (sp->top==sp->size-1);
+}
+int main(){
+
+    return 0;
+}
