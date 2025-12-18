15c0b9216285c70ba6812f2b681c57d0a548eb08
Pritam-coder969 <pdascse24@gmail.com>
2025-12-18 16:22:45 +0000

    Dec 18, 2025, 9:52 PM

--- c/Code_With_Harry_C_Tutorials/infixToPostfixPrac.c
+++ c/Code_With_Harry_C_Tutorials/infixToPostfixPrac.c
@@ -47,6 +47,7 @@ int isOperator(char ch){
 int precedence(char ch){
     if(ch=='+' || ch=='-') return 1;
     else if(ch=='*' || ch=='/') return 2;
+    else if(ch=='^') return 3;
     else return 0;
 }
 
