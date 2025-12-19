a856e6cc2927e064bfe4dc094283313b140d56a0
Pritam-coder969 <pdascse24@gmail.com>
2025-12-19 02:30:13 +0000

    Dec 19, 2025, 8:00 AM

--- c/DSA_Assignment1/Q6.c
+++ c/DSA_Assignment1/Q6.c
@@ -1,1 +1,10 @@
 // Write a program in C to evaluate value of a postfix expression using stack. E.g. 456*+ result will be 34.
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack{
+    int top;
+    int size;
+    int* arr;
+}stack;
+
