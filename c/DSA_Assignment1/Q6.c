4b65c523571aa16db56c23ca71714a6f38365a77
Pritam-coder969 <pdascse24@gmail.com>
2025-12-19 02:30:54 +0000

    Dec 19, 2025, 8:00 AM

--- c/DSA_Assignment1/Q6.c
+++ c/DSA_Assignment1/Q6.c
@@ -8,3 +8,34 @@ typedef struct stack{
     int* arr;
 }stack;
 
+int stackTop(stack* sp){
+    if(sp->top==-1) return -1;
+    return sp->arr[sp->top];
+}
+
+int isEmpty(stack* sp){
+    return (sp->top==-1);
+}
+
+int isFull(stack* sp){
+    return (sp->top==sp->size-1);
+}
+
+void push(stack* sp,char val){
+    if(isFull(sp)){
+        printf("Stack Overflow! Can't push %c into stack\n",val);
+        return;
+    }
+    sp->top++;
+    sp->arr[sp->top]=val;
+}
+
+char pop(stack* sp){
+    if(isEmpty(sp)){
+        printf("Stack Underflow! Can't pop from stack\n");
+        return -1;
+    }
+    char val=sp->arr[sp->top];
+    sp->top--;
+    return val;
+}
