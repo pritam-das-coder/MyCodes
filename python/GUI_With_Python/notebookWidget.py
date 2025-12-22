2b3489ac7203840c76bb1fa6ede80632a86536dd
Pritam-coder969 <pdascse24@gmail.com>
2025-12-22 14:43:51 +0000

    Dec 22, 2025, 8:13 PM

--- python/GUI_With_Python/notebookWidget.py
+++ python/GUI_With_Python/notebookWidget.py
@@ -11,5 +11,8 @@ my_notebook.pack()
 my_frame1=Frame(my_notebook,width=500,height=500,bg="blue")
 my_frame2=Frame(my_notebook,width=500,height=500,bg="red")
 
-my_frame1.pack()
+my_frame1.pack(fill="both",expand=True)
+my_frame2.pack(fill="both",expand=True)
+
+
 root.mainloop()
