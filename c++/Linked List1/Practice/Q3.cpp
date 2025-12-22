70d39251bcc16a7f716509346f18f89185cde27b
Pritam-coder969 <pdascse24@gmail.com>
2025-12-22 08:19:12 +0000

    Dec 22, 2025, 1:49 PM

--- c++/Linked List1/Practice/Q3.cpp
+++ c++/Linked List1/Practice/Q3.cpp
@@ -1,3 +1,25 @@
 // Implement a Linked List class.
 // The user defined LL should have insert (head,tail,idx) , delete(head,tail,idx) , get(idx) and display
 // functions.
+#include <iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+class LL{
+public:
+    Node* head;
+    Node* tail;
+    int size;
+    LL(){
+        head=tail=NULL;
+        size=0;
+    }
+    
+};
