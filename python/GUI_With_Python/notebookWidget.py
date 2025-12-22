123860f9ff0e15930c714745be8312f72eb590c3
Pritam-coder969 <pdascse24@gmail.com>
2025-12-22 14:45:48 +0000

    Dec 22, 2025, 8:15 PM

--- python/GUI_With_Python/notebookWidget.py
+++ python/GUI_With_Python/notebookWidget.py
@@ -14,5 +14,7 @@ my_frame2=Frame(my_notebook,width=500,height=500,bg="red")
 my_frame1.pack(fill="both",expand=True)
 my_frame2.pack(fill="both",expand=True)
 
+my_notebook.add(my_frame1,text="Blue Tab")
+my_notebook.add(my_frame2,text="Red Tab")
 
 root.mainloop()
