7198358e430cdbff888f0b85a965b47db4687e85
Pritam-coder969 <pdascse24@gmail.com>
2025-12-22 14:38:49 +0000

    Dec 22, 2025, 8:08 PM

---
+++ python/GUI_With_Python/notebookWidget.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+root=Tk()
+root.title("Learn Notebook")
+root.geometry("500x500")
+
+my_notebook=ttk.Notebook(root)
+my_notebook.pack()
+
+
+root.mainloop()
