52d5d02d642f78ab4039601a9d040a1f2fe61b88
Pritam-coder969 <pdascse24@gmail.com>
2025-12-27 03:18:52 +0000

    Dec 27, 2025, 8:48 AM

--- c++/Practice/mergeSort2.cpp
+++ c++/Practice/mergeSort2.cpp
@@ -5,7 +5,15 @@ void sort(vector<int>& v){
     int n=v.size();
     vector<int> a(n/2);
     vector<int> b(n-n/2);
-    int i=0;
+    int i=0,j=0;
+    while(i<n/2){
+        a[i]=v[i];
+        i++;
+    }
+    while(i<n){
+        b[j]=v[i];
+        i++;
+    }
     
 }
 int main(){
