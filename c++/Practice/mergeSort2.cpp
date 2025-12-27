77f402c9f583c719b97624f322323a8b7b4dfe78
Pritam-coder969 <pdascse24@gmail.com>
2025-12-27 03:37:39 +0000

    Dec 27, 2025, 9:07 AM

--- c++/Practice/mergeSort2.cpp
+++ c++/Practice/mergeSort2.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 void sort(vector<int>& v){
     int n=v.size();
+    if(n==1 || n==0) return;
     vector<int> a(n/2);
     vector<int> b(n-n/2);
     int i=0,j=0;
@@ -17,9 +18,39 @@ void sort(vector<int>& v){
     }
     sort(a);
     sort(b);
+    i=0,j=0;
+    int k=0;
+    while(i<n/2 && j<(n-n/2)){
+        if(a[i]<=b[j]){
+            v[k]=a[i];
+            k++;
+            i++;
+        }
+        else{
+            v[k]=b[j];
+            k++;
+            j++;
+        }
+    }
+    while(i<a.size()){
+        v[k]=a[i];
+        i++;
+        k++;
+    }
+    while(j<b.size()){
+        v[k]=b[j];
+        j++;
+        k++;
+    }
     
-
 }
 int main(){
-
+    int arr[]={7,1,2,-1,0,6,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    sort(v);
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
 }
