7a1ae4b9613912129e7ca3a0a854dfd5d51f0538
Pritam-coder969 <pdascse24@gmail.com>
2025-12-29 14:33:41 +0000

    Dec 29, 2025, 8:03 PM

---
+++ c++/Linked List2/doublyLL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+
