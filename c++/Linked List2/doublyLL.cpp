285223cd5666264f5f444f64f9225e584a3694ac
Pritam-coder969 <pdascse24@gmail.com>
2025-12-29 14:39:02 +0000

    Dec 29, 2025, 8:09 PM

--- c++/Linked List2/doublyLL.cpp
+++ c++/Linked List2/doublyLL.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Node{
+public:
     int val;
     Node* next;
     Node* prev;
@@ -12,3 +13,25 @@ class Node{
     }
 };
 
+Node* build123(){
+    Node* a=new Node(1);
+    Node* b=new Node(2);
+    Node* c=new Node(3);
+    a->next=b;
+    b->next=c;
+    b->prev=a;
+    c->prev=b;
+    return a;
+}
+
+void display(Node* head){
+    while(head){
+        cout<<head->val<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Node* dll=build123();
+    display(dll);
+}
