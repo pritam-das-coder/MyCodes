d4431c60f2fcbbe774548601790dd88141d4483b
Pritam-coder969 <pdascse24@gmail.com>
2025-12-29 18:58:20 +0000

    Dec 30, 2025, 12:28 AM

--- c++/Linked List2/doublyLLClass.cpp
+++ c++/Linked List2/doublyLLClass.cpp
@@ -32,39 +32,15 @@ public:
         }
         size++;
     }
+    void display(Node* head){
+        while(head){
+            cout<<head->val<<" ";
+            head=head->next;
+        }
+        cout<<endl;
+    }
 };
-void display(Node* head){
-    while(head){
-        cout<<head->val<<" ";
-        head=head->next;
-    }
-    cout<<endl;
-}
-
-void displayRev(Node* tail){
-    while(tail){
-        cout<<tail->val<<" ";
-        tail=tail->prev;
-    }
-    cout<<endl;
-}
 
 int main(){
-    // 10 20 30 40 50
-    // a  b  c  d  e
-    Node* a=new Node(10);
-    Node* b=new Node(20);
-    Node* c=new Node(30);
-    Node* d=new Node(40);
-    Node* e=new Node(50);
-    a->next=b;
-    b->next=c;
-    b->prev=a;
-    c->prev=b;
-    c->next=d;
-    d->next=e;
-    d->prev=c;
-    e->prev=d;
-    displayRev(e);
-    display(a);
+    
 }
