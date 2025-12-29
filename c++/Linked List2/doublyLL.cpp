a0f9a114e85838b5d837d67066d39f7d2f0026df
Pritam-coder969 <pdascse24@gmail.com>
2025-12-29 14:42:56 +0000

    Dec 29, 2025, 8:12 PM

--- c++/Linked List2/doublyLL.cpp
+++ c++/Linked List2/doublyLL.cpp
@@ -13,14 +13,21 @@ public:
     }
 };
 
-Node* build123(){
-    Node* a=new Node(1);
-    Node* b=new Node(2);
-    Node* c=new Node(3);
+Node* buildDLL(){
+    // 10 20 30 40 50
+    Node* a=new Node(10);
+    Node* b=new Node(20);
+    Node* c=new Node(30);
+    Node* d=new Node(40);
+    Node* e=new Node(50);
     a->next=b;
     b->next=c;
     b->prev=a;
     c->prev=b;
+    c->next=d;
+    d->next=e;
+    d->prev=c;
+    e->prev=d;
     return a;
 }
 
@@ -32,6 +39,6 @@ void display(Node* head){
     cout<<endl;
 }
 int main(){
-    Node* dll=build123();
+    Node* dll=buildDLL();
     display(dll);
 }
