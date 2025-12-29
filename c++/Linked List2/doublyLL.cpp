35f01c184cb79f8ff52cf315be6398f2dca01a15
Pritam-coder969 <pdascse24@gmail.com>
2025-12-29 18:35:06 +0000

    Dec 30, 2025, 12:05 AM

--- c++/Linked List2/doublyLL.cpp
+++ c++/Linked List2/doublyLL.cpp
@@ -44,10 +44,22 @@ void displayRev(Node* tail){
         cout<<tail->val<<" ";
         tail=tail->prev;
     }
+    cout<<endl;
 }
 
 int main(){
-    Node* dll=buildDLL();
-    //display(dll);
-    displayRev(dll);
+    // 10 20 30 40 50
+    Node* a=new Node(10);
+    Node* b=new Node(20);
+    Node* c=new Node(30);
+    Node* d=new Node(40);
+    Node* e=new Node(50);
+    a->next=b;
+    b->next=c;
+    b->prev=a;
+    c->prev=b;
+    c->next=d;
+    d->next=e;
+    d->prev=c;
+    e->prev=d;
 }
