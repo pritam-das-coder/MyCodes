3ec63c1bfcf145ee08b07c60ffb11740c369cbed
Pritam-coder969 <pdascse24@gmail.com>
2025-12-30 06:50:23 +0000

    Dec 30, 2025, 12:20 PM

---
+++ c++/Linked List2/circularDLL.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+void display(Node* head){
+    Node* temp=head;
+    do{
+        cout<<temp->val<<endl;
+        temp=temp->next;
+    }while(temp!=head);
+    cout<<endl;
+}
+int main(){
+    // 10 20 30 40
+    // a  b  c  d
+    Node* a=new Node(10);
+    Node* b=new Node(20);
+    Node* c=new Node(30);
+    Node* d=new Node(40);
+    a->next=b;
+    a->prev=d;
+    b->next=c;
+    b->prev=a;
+    c->prev=b;
+    c->next=d;
+    d->next=a;
+    d->prev=c;
+    
+    Node* head=a;
+    Node* tail=d;
+    display(head);
+
+    Node* ptr=new Node(50);
+    tail->next=ptr;
+    head->prev=ptr;
+    ptr->prev=tail;
+    ptr->next=head;
+    display(head);
+}
