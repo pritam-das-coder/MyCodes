c62e90086f8b009427bd69983c7e784c51c42089
Pritam-coder969 <pdascse24@gmail.com>
2026-01-25 06:12:24 +0000

    Jan 25, 2026, 11:42 AM

---
+++ c++/Stacks/reverseStack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+    stack<int> st;
+    stack<int> temp;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    
+    
+
+
+    return 0;
+}
