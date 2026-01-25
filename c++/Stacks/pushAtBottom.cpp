6fdd9fadba3ff77a23d2e3344b659278f810d7be
Pritam-coder969 <pdascse24@gmail.com>
2026-01-25 13:43:24 +0000

    Jan 25, 2026, 7:13 PM

---
+++ c++/Stacks/pushAtBottom.cpp
@@ -0,0 +1,27 @@
+// Normal Print
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+    stack<int> st;
+    stack<int> temp;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    // st to temp
+    while(st.size()){
+        int x=st.top();
+        st.pop();
+        temp.push(x);
+    }
+    // temp to st
+    while(temp.size()){
+        int x=temp.top();
+        cout<<x<<" ";
+        temp.pop();
+        st.push(x);
+    }
+    cout<<endl<<st.size()<<endl;
+    return 0;
+}
