086a868981b4e3ffb25f287037b58701926be8a1
Pritam-coder969 <pdascse24@gmail.com>
2026-01-25 06:18:51 +0000

    Jan 25, 2026, 11:48 AM

--- c++/Stacks/reverseStack.cpp
+++ c++/Stacks/reverseStack.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-void transfer(stack<int> a,stack<int> b){ // a to b
+void transfer(stack<int>& a,stack<int>& b){ // a to b
     while(a.size()){
         b.push(a.top());
         a.pop();
@@ -17,7 +17,9 @@ int main(){
     st.push(40);
     st.push(50);
     
-
+    transfer(st,t1);
+    transfer(t1,t2);
+    transfer(t2,st);
 
 
     return 0;
