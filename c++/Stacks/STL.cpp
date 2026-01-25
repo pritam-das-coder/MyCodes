57b72b7baf5e9b833ea51f0d952b6938575592cd
Pritam-coder969 <pdascse24@gmail.com>
2026-01-25 04:50:16 +0000

    Jan 25, 2026, 10:20 AM

--- c++/Stacks/STL.cpp
+++ c++/Stacks/STL.cpp
@@ -8,9 +8,10 @@ int main(){
     st.push(20);
     st.push(30);
     st.push(40);
-    cout<<st.size()<<endl;
-    st.pop();
-    cout<<st.size()<<endl;
-    cout<<st.top()<<endl;
+    // cout<<st.size()<<endl;
+    // st.pop();
+    // cout<<st.size()<<endl;
+    // cout<<st.top()<<endl;
+    
     return 0;
 }
