c2d447e777926944a8b316316a59ce57b0d3c0e2
Pritam-coder969 <pdascse24@gmail.com>
2026-01-26 14:24:40 +0000

    Jan 26, 2026, 7:54 PM

---
+++ c++/Stacks/copyStack.cpp
@@ -0,0 +1,48 @@
+// Copy stack into another stack in same order 
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+void print(stack<int>& st){
+    stack<int> temp;
+    // st to temp
+    while(st.size()){
+        int x=st.top();
+        st.pop();
+        temp.push(x);
+    }
+    // temp to st
+    while(temp.size()){
+        int x=temp.top();
+        cout<<x<<" ";
+        temp.pop();
+        st.push(x);
+    }
+    cout<<endl;
+}
+int main(){
+    stack<int> st;
+    vector<int> v;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    
+    print(st);
+
+    while(st.size()){
+        int x=st.top();
+        st.pop();
+        v.push_back(x);
+    }
+    int i=0;
+    while(i<v.size()){
+        st.push(v[i]);
+        i++;
+    }
+
+    print(st);
+
+    return 0;
+}
