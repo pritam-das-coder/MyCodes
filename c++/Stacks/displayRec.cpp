823c90e30fc81c28438f44b543ce9829a3e983eb
Pritam-coder969 <pdascse24@gmail.com>
2026-01-26 15:36:47 +0000

    Jan 26, 2026, 9:06 PM

---
+++ c++/Stacks/displayRec.cpp
@@ -0,0 +1,26 @@
+// Copy stack into another stack in same order 
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+void printRec(stack<int>& st){
+    if(st.size()==0) return;
+    int x=st.top();
+    cout<<x<<" ";
+    st.pop();
+    printRec(st);
+    st.push(x);
+}
+int main(){
+    stack<int> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    
+    printRec(st);
+
+    return 0;
+}
