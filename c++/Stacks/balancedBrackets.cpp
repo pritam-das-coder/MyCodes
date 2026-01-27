609710c7d8f930b941c5fdefc0a886152600feee
Pritam-coder969 <pdascse24@gmail.com>
2026-01-27 12:04:36 +0000

    Jan 27, 2026, 5:34 PM

--- c++/Stacks/balancedBrackets.cpp
+++ c++/Stacks/balancedBrackets.cpp
@@ -3,7 +3,16 @@
 #include <string>
 using namespace std;
 void check(string s){
-    
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        char ch=s[i];
+        if(ch=='('){
+            st.push(ch);
+        }
+        else{
+            st.pop();
+        }
+    }
 }
 int main(){
     
