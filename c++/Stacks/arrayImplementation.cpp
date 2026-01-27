20a1788bd27072b1f702765a7cced291c73f8528
Pritam-coder969 <pdascse24@gmail.com>
2026-01-27 03:52:03 +0000

    Jan 27, 2026, 9:22 AM

---
+++ c++/Stacks/arrayImplementation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Stack{
+public:
+    int arr[10];
+    int idx;
+    void push(int val){
+        if(idx>=9){
+            cout<<"Stack is Full!"<<endl;
+            return;
+        }
+        idx++;
+        arr[idx]=val;
+    }
+    void pop(){
+        if(idx<=0){
+            cout<<"Stack is Empty"<<endl;
+            return;
+        }
+        idx--;
+    }
+    int top(){
+        return arr[idx];
+    }
+};
+int main(){
+    
+    return 0;
+}
