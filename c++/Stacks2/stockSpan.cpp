5477c84aaa3a85fd9662969847075ae0175a62c5
Pritam-coder969 <pdascse24@gmail.com>
2026-01-28 07:07:12 +0000

    Jan 28, 2026, 12:37 PM

--- c++/Stacks2/stockSpan.cpp
+++ c++/Stacks2/stockSpan.cpp
@@ -12,23 +12,23 @@ int main(){
 
     // finding ans array
     // SC=O(n) TC=O(n)
-    int ans[n];
+    int pgi[n];
     stack<int> st;
-    ans[0]=1;
+    pgi[0]=-1;
     st.push(0);
     for(int i=1;i<n;i++){
         while(st.size() && arr[st.top()]<=arr[i]) st.pop();
-        if(st.size()==0) ans[i]=1;
-        else ans[i]=st.top();
+        if(st.size()==0) pgi[i]=1;
+        else pgi[i]=st.top();
         st.push(i);
     }
+    int span[n];
     for(int i=0;i<n;i++){
-        if(ans[i]==1) continue;
-        ans[i]=i-ans[i];
+        span[i]=i-pgi[i];
     }
 
     for(int i=0;i<n;i++){
-        cout<<ans[i]<<" ";
+        cout<<span[i]<<" ";
     }
     cout<<endl;
 }
