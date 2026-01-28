088bfaaaec96a427ac4ebc6843dd27cfdc9a99e1
Pritam-coder969 <pdascse24@gmail.com>
2026-01-28 04:33:28 +0000

    Jan 28, 2026, 10:03 AM

--- c++/Stacks2/nextGreater.cpp
+++ c++/Stacks2/nextGreater.cpp
@@ -3,11 +3,12 @@
 #include <string>
 using namespace std;
 int main(){
-    int arr[]={3,1,2,5,4,6,2,3};
+    int arr[]={3,1,7,5,4,9,2,3};
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
     int ans[n];
     stack<int> st;
     ans[n-1]=-1;
@@ -21,4 +22,5 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<ans[i]<<" ";
     }
+    cout<<endl;
 }
