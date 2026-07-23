666b8c1032af27ee770875ac58171de8c3ccc6e7
Pritam-coder969 <pdascse24@gmail.com>
2026-07-23 13:43:48 +0530

    July 23

---
+++ java/Type_Conversion/typeConversion1.java
@@ -0,0 +1,18 @@
+public class typeConversion1 {
+    public static void main(String[] args) {
+        // byte a = 50;
+        // byte b = 40;
+        // byte c = 100;
+        // int i = (a*b)/c;
+        // System.out.println(i);
+
+        // byte b = 50;
+        // b =(byte) (b*2);
+        // System.out.println(b);
+
+        char a = 'A';
+        char b = 'B';
+        char c = (char)(a+b);
+        System.out.println(c);
+    }
+}
