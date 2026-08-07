5565d57f115b90d82499df145040c4ec077b4941
Pritam-coder969 <pdascse24@gmail.com>
2026-08-07 09:10:31 +0530

    Aug 7

---
+++ java/Arrays/memoryRep.java
@@ -0,0 +1,10 @@
+public class memoryRep {
+    public static void main(String[] args) {
+        // int arr[]=new int[5];
+        // System.out.println(arr);
+        // System.out.println(arr[5]);
+
+        int arr[][]=new int[3][4];
+        System.out.println(arr[0][1]);// [I@4517d9a3 [I@4517d9a3
+    }
+}
